c51a6698d553d59b4a2ca2a940c91aae76584f0a
GadgetBuilder <GadgetBuild@gmail.com>
2015-11-10 10:22:04 -0600

    Adding Java24 path on tablet

---
+++ src/com/Java24Local/Variable.java
@@ -0,0 +1,20 @@
+package com.Java24Local;
+
+public class Variable {
+
+	public static void main(String[] arguments) {
+		int tops;		
+		float gradePointAverage;		
+		char key = 'C';
+		String productName = "Larvets";
+		byte escapeKey = 27;
+		short roomNumber = 222;
+		long salary = 264_400_000;
+		boolean gameOver = false;
+		
+		
+		
+
+	}
+
+}
