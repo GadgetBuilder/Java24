03217c9a6e0081971b5548f8776a07a3da752db9
GadgetBuilder <GadgetBuild@gmail.com>
2015-11-12 20:34:33 -0600

    Finished chapter 5

---
+++ src/com/Java24Local/XandYSquare.java
@@ -0,0 +1,13 @@
+package com.Java24Local;
+
+public class XandYSquare {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		int tolalOfNumers = (x * x) + (y * y);
+		System.out.println(" The answer is " + tolalOfNumers);
+
+	}
+
+}
