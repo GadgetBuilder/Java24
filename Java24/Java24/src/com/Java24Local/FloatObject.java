c5eb368b52a6cbe133f35b7e413df11c85c0f67a
GadgetBuilder <GadgetBuild@gmail.com>
2015-12-07 20:44:16 -0600

    Finished chapter 11

---
+++ Java24/Java24/src/com/Java24Local/FloatObject.java
@@ -0,0 +1,14 @@
+package com.Java24Local;
+
+public class FloatObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Float Object is " + ConvertFloat.class) ;
+		
+		ConvertFloat FloatObject = new ConvertFloat();
+		System.out.println("End Value is " + ConvertFloat.class); 
+
+	}
+
+}
